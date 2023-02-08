ae7d7c93f165601c18f84310949b16f98002b4ce
Saoa35 <saoamah@gmail.com>
2023-02-08 19:31:08 +0100

    + Something went wrong  text

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,5 +2,11 @@ import { useSearchUsersQuery } from "../store/github/github.api";
 
 export function HomePage() {
   const { isLoading, isError, data } = useSearchUsersQuery("Saoa35");
-  return <div>Home</div>;
+  return (
+    <div>
+      {isError && (
+        <p className="text-center text-red-600">Something went wrong</p>
+      )}
+    </div>
+  );
 }
