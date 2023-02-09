b5592506c4181306c38ba8c3b078831e3fa6a514
Saoa35 <saoamah@gmail.com>
2023-02-09 21:46:05 +0100

    + input

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -7,6 +7,14 @@ export function HomePage() {
       {isError && (
         <p className="text-center text-red-600">Something went wrong</p>
       )}
+
+      <div className="relative w-[560px]">
+        <input
+          type="text"
+          className="border py-2 px-4 w-full h-[42px] mb-2"
+          placeholder="Search for Github Username..."
+        />
+      </div>
     </div>
   );
 }
