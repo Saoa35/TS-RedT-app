dfd03d4caf70e84810e1d8458088e715bf20637c
Saoa35 <saoamah@gmail.com>
2023-02-09 21:48:42 +0100

    + useState

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,9 @@
+import { useState } from "react";
 import { useSearchUsersQuery } from "../store/github/github.api";
 
 export function HomePage() {
   const { isLoading, isError, data } = useSearchUsersQuery("Saoa35");
+  const [search, setSearch] = useState();
   return (
     <div>
       {isError && (
